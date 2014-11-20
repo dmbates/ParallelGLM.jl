4a5e3b6749c96a724801f98d1d76140ea52f520a
Douglas Bates <dmbates@gmail.com>
2014-11-20 15:57:53 -0600

    Using anonymous functions and function members.
    This is too slow.  Will need to compile-in particular the member functions for particular combinations.

--- src/irls.jl
+++ src/irls.jl
@@ -13,25 +13,27 @@ Evaluate `y*log(y/μ)` with the correct limit as `y` approaches zero from above
 ylogydμ{T<:FloatingPoint}(y::T,μ::T) = y > zero(T) ? y*log(y/μ) : zero(T)
 
 @doc """
-Evaluate the variance function for distribution instance and value of `μ`
+The Bernoulli and binomial variance function
 """->
-Base.var{T<:FloatingPoint}(::Bernoulli,μ::T) = μ*(one(T)-μ)
-Base.var{T<:FloatingPoint}(::Binomial,μ::T) = μ*(one(T)-μ)
-Base.var{T<:FloatingPoint}(::Gamma,μ::T) = abs2(μ)
-Base.var{T<:FloatingPoint}(::Normal,μ::T) = one(T)
-Base.var{T<:FloatingPoint}(::Poisson,μ::T) = μ
+bernvar(μ) = μ*(one(μ)-μ)
+
+varfunc(::Bernoulli) = bernvar
+varfunc(::Binomial) = bernvar
+varfunc(::Gamma) = abs2
+varfunc(::Normal) = one
+varfunc(::Poisson) = id
 
 @doc """
 Evaluate half the squared deviance residual for a distribution instance and values of `y` and `μ`
 """->
-devresid2{T<:FloatingPoint}(::Bernoulli,y::T,μ::T) = ylogydμ(y,μ) + ylogydμ(one(T)-y,one(T)-μ)
-devresid2{T<:FloatingPoint}(::Binomial,y::T,μ::T) = devresid2(Bernoulli(),y,μ)
-devresid2{T<:FloatingPoint}(::Gamma,y::T,μ::T) = (y-μ)/μ - (y == zero(T) ? y : log(y/μ))
-devresid2{T<:FloatingPoint}(::Normal,y::T,μ::T) = abs2(y-μ)
-devresid2{T<:FloatingPoint}(::Poisson,y::T,μ::T) = ylogydμ(y,μ)-(y-μ)
+devresid2(::Bernoulli) = (y,μ) -> 2*(ylogydμ(y,μ) + ylogydμ(one(y)-y,one(μ)-μ))
+devresid2(::Binomial) = devresid2(Bernoulli())
+devresid2(::Gamma) = (y,μ) -> (y-μ)/μ - (y == zero(T) ? y : log(y/μ))
+devresid2(::Normal) = (y,μ) -> abs2(y-μ)
+devresid2(::Poisson) = (y,μ) -> 2*(ylogydμ(y,μ)-(y-μ))
 
 @doc "Representation of a generalized linear model using SharedArrays" ->
-type PGLM{T<:FloatingPoint,D<:UnivariateDistribution,L<:Link}
+type PGLM{T<:FloatingPoint}
     Xt::SharedMatrix{T}
     wt::SharedVector{T}
     y::SharedVector{T}
@@ -40,64 +42,60 @@ type PGLM{T<:FloatingPoint,D<:UnivariateDistribution,L<:Link}
     δβ::SharedVector{T}
     η::SharedVector{T}
     μ::SharedVector{T}
-    d::D
-    l::L
+    dev::SharedVector{T}
+    link::Function
+    invlink::Function
+    μη::Function
+    varfunc::Function
+    devresid2::Function
     canon::Bool
     fit::Bool
 end
 
-function PGLM{T<:FloatingPoint,D<:UnivariateDistribution,
-              L<:Link}(Xt::SharedMatrix{T},
-                       y::SharedVector{T},
-                       wt::SharedVector{T},
-                       d::D,
-                       l::L)
+function PGLM{T<:FloatingPoint}(Xt::SharedMatrix{T},
+                                y::SharedVector{T},
+                                wt::SharedVector{T},
+                                d::UnivariateDistribution,
+                                l::Link)
     p,n = size(Xt)
-    workrs = procs(y)
     n == length(y) || throw(DimensionMismatch(""))
     (lw = length(wt)) == 0 || lw == n || throw(DimensionMismatch(""))
-    length(intersect(workrs,procs(Xt))) == length(workrs) ||
-        error("worker procs for y and Xt are not identical")
-    PGLM(Xt,wt,y,fill!(similar(y,(p,)),zero(T)),fill!(similar(y,(p,)),zero(T)),
-         fill!(similar(y,(p,)),zero(T)),similar(y),similar(y),d,l,l==canonical(d),false)
+    pr = procs(y)
+    Set(pr) == Set(procs(Xt)) == Set(procs(wt)) || error("SharedArrays must have same procs")
+    β = Base.shmem_fill(zero(T),(p,);pids = pr)
+    ntot = maximum(pr)
+    PGLM(Xt,wt,y,β,copy(β),copy(β),similar(y),similar(y),
+         similar(y,(ntot,)),link(l),invlink(l),μη(l),
+         varfunc(d),devresid2(d),l==canonical(d),false)
 end
-function PGLM{T<:FloatingPoint,D<:UnivariateDistribution,
-              L<:Link}(Xt::SharedMatrix{T},
-                       y::SharedVector{T},
-                       d::D,
-                       l::L)
+function PGLM{T<:FloatingPoint}(Xt::SharedMatrix{T},y::SharedVector{T},
+                                d::UnivariateDistribution,l::Link)
     PGLM(Xt,y,similar(y,(0,)),d,l)
 end
-function PGLM{T<:FloatingPoint,D<:UnivariateDistribution}(Xt::SharedMatrix{T},
-                                                y::SharedVector{T},
-                                                d::D)
+function PGLM{T<:FloatingPoint}(Xt::SharedMatrix{T},
+                                y::SharedVector{T},
+                                d::UnivariateDistribution)
     PGLM(Xt,y,d,canonical(d))
 end
 
 @doc """
 Evaluate the sum of the squared deviance residuals on the local indices of `y`
 """ ->
-function locdev!{T<:FloatingPoint}(η::SharedVector{T},
-                                   μ::SharedVector{T},
-                                   Xt::SharedMatrix{T},
-                                   y::SharedVector{T},
-                                   wt::SharedVector{T},
-                                   β::SharedVector{T},
-                                   d::UnivariateDistribution,
-                                   l::Link)
+
+function loc_dev!{T<:FloatingPoint}(g::PGLM{T})
+    usewt = length(g.wt) > 0
     dev = zero(T)
-    usewt = length(wt) > 0
-    @inbounds for j in localindexes(y)
+    @inbounds for j in localindexes(g.y)
         sm = zero(T)
-        @simd for k in 1:length(β)
-            sm += Xt[k,j]*β[k]
+        @simd for k in 1:length(g.β)
+            sm += g.Xt[k,j]*g.β[k]
         end
-        η[j] = sm
-        μ[j] = invlink(l,sm)
-        dr2 = devresid2(d,y[j],μ[j])
-        dev += usewt ? wt[j] * dr2 : dr2
+        g.η[j] = sm
+        g.μ[j] = g.invlink(sm)
+        dr2 = g.devresid2(g.y[j],g.μ[j])
+        dev += usewt ? g.wt[j] * dr2 : dr2
     end
-    2dev
+    g.dev[g.y.pidx] = dev
 end
 
 @doc """
@@ -105,13 +103,54 @@ Evaluate the deviance for `PGLM` model `g` using step factor `s`
 
 This method also updates the `η` and `μ` members of `g`
 """->
-function StatsBase.deviance{T<:FloatingPoint}(g::PGLM,s::T)
+function StatsBase.deviance{T<:FloatingPoint}(g::PGLM{T},s::T)
     @simd for k in 1:length(g.β)
         @inbounds g.β[k] = g.β₀[k] + s * g.δβ[k]
     end
-    dev = zeros(T,(length(procs(g.y)+1),))
+    fill!(g.dev,zero(T))
     @sync for p in procs(g.y)
-        @async dev[p] = remotecall_fetch(p,locdev!,g.η,g.μ,g.Xt,g.y,g.wt,g.β,g.d,g.l)
+        @async remotecall_wait(p,loc_dev!,g)
     end
-    sum(dev)
+    sum(g.dev)
 end
+
+function dev1proc{T<:FloatingPoint}(g::PGLM{T},s::T)
+    @simd for k in 1:length(g.β)
+        @inbounds g.β[k] = g.β₀[k] + s * g.δβ[k]
+    end
+    usewt = length(g.wt) > 0
+    dev = zero(T)
+    for j in 1:length(g.y)
+        sm = zero(T)
+        @simd for k in 1:length(g.β)
+            sm += g.Xt[k,j]*g.β[k]
+        end
+        g.η[j] = sm
+        g.μ[j] = g.invlink(sm)
+        dr2 = g.devresid2(g.y[j],g.μ[j])
+        dev += usewt ? g.wt[j] * dr2 : dr2
+    end
+    dev
+end
+
+function dev1procinline{T<:FloatingPoint}(g::PGLM{T},s::T)
+    @simd for k in 1:length(g.β)
+        @inbounds g.β[k] = g.β₀[k] + s * g.δβ[k]
+    end
+    usewt = length(g.wt) > 0
+    dev = zero(T)
+    for j in 1:length(g.y)
+        sm = zero(T)
+        @simd for k in 1:length(g.β)
+            sm += g.Xt[k,j]*g.β[k]
+        end
+        g.η[j] = sm
+        g.μ[j] = logistic(sm)
+        dr2 = 2*(ylogydμ(g.y[j],g.μ[j]) + ylogydμ(one(T)-g.y[j],one(T)-g.μ[j]))
+        dev += usewt ? g.wt[j] * dr2 : dr2
+    end
+    dev
+end
+
+### Create a macro to cause compilation of the loc_dev function with explicit substitution of
+### linkinv and devresid2 for particular combinations of D and L
--- src/link.jl
+++ src/link.jl
@@ -1,42 +1,29 @@
 abstract Link
 
-type IdentityLink <: Link end
-type InverseLink <: Link end
-type LogLink <: Link end
-type LogitLink <: Link end
+immutable IdentityLink <: Link end
+immutable InverseLink <: Link end
+immutable LogLink <: Link end
+immutable LogitLink <: Link end
 
-link{T<:FloatingPoint}(::IdentityLink,μ::T) = μ
-invlink{T<:FloatingPoint}(::IdentityLink,η::T) = η
-μη{T<:FloatingPoint}(::IdentityLink,η::T) = one(T)
+logit(μ) = log(μ/(one(μ)-μ))
+logistic(η) = inv(one(η) + exp(-η))
+logisticder(η) = (ee = exp(-η); ee/abs2(one(η)+ee))
+id(x) = x
+ninvabs2(η) = -inv(abs2(η))
 
-link{T<:FloatingPoint}(::LogLink,μ::T) = log(μ)
-invlink{T<:FloatingPoint}(::LogLink,η::T) = exp(η)
-μη{T<:FloatingPoint}(::LogLink,η::T) = exp(η)
+link(::IdentityLink) = id
+invlink(::IdentityLink) = id
+μη(::IdentityLink) = one
 
-link{T<:FloatingPoint}(::LogitLink,μ::T) = log(μ/(one(T) - μ))
-invlink{T<:FloatingPoint}(::LogitLink,η::T) = inv(one(T) + exp(-η))
-μη{T<:FloatingPoint}(::LogitLink,η::T) = (μ = invlink(LogitLink(),η); μ*(one(T) - μ))  
+link(::LogitLink) = logit
+invlink(::LogitLink) = logistic
+μη(::LogitLink) = logisticder
 
-function linkl!{T<:FloatingPoint}(η::SharedArray{T},l::Link,μ::SharedArray{T})
-    for j in localindexes(μ)
-        η[j] = link(l,μ)
-    end
-end
-function invlinkl!{T<:FloatingPoint}(μ::SharedArray{T},l::Link,η::SharedArray{T})
-    for j in localindexes(η)
-        μ[j] = invlink(l,η)
-    end
-end
-function μηl!{T<:FloatingPoint}(mueta::SharedArray{T},l::Link,η::SharedArray{T}) 
-    for j in localindexes(η)
-        mueta[j] = μη(l,η)
-    end
-end
-function link!{T<:FloatingPoint}(η::SharedArray{T},l::Link,μ::SharedArray{T})
-    length(η) == length(μ) || throw(DimensionMismatch(""))
-    @sync for p in procs(μ)
-         @async remotecall_wait(p,linkl!,η,l,μ)
-    end
-    η
-end
-link{T<:FloatingPoint}(l::Link,μ::SharedArray{T}) = link!(similar(μ),l,μ)
+link(::LogLink) = log
+invlink(::LogLink) = exp
+μη(::LogLink) = exp
+
+link(::InverseLink) = inv
+invlink(::InverseLink) = inv
+μη(::InverseLink) = ninvabs2
+
